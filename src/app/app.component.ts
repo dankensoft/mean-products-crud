c4393585c2813808664e543fa04894f567f59063
Danniels Castillo <danniels.magana@gmail.com>
2020-12-27 19:01:17 -0600

    CParte 1: Configurando Node, Express y Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatusService } from './shared/status.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mean-products';
+  status = 'DOWN DKS SERVER';
+
+  constructor( private _statusService: StatusService ){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this._statusService
+      .getStatus()
+      .then((result: any) => {
+        this.status = result.status;
+      });
+  }
 }
